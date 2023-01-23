1a6f1c5408f0e67cd17a1ca2497a1f135a39c1e9
Dorkin Kirill <77581111+Kirilldorkin@users.noreply.github.com>
2023-01-23 14:38:55 +0600

    Add files via upload

---
+++ Дз1/main.js
@@ -0,0 +1,5 @@
+const NAME = "Kirill";
+const SURNAME = "Dorkin";
+
+console.log(`Hi my dear ${NAME} ${SURNAME}.`);
+alert(`Hi my dear ${NAME} ${SURNAME}.`);
---
+++ Дз3/main.js
@@ -0,0 +1,19 @@
+let color = prompt('red || yellow || green');
+
+if (color === "red")
+{
+    console.log("стоять");
+    alert("стоять");
+}
+
+if (color === "yellow")
+{
+    console.log("ждать");
+    alert("ждать");
+}
+
+if (color === "green")
+{
+    console.log("идти");
+    alert("идти");
+}
